05d1675700f4c7a690d350d231ad4ea8405f813a
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-05-25 20:26:33 +0800

    created all my CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # read all tasks
+  get 'tasks', to: 'tasks#index'
+  # read one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # create one task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # update one task
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete one task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
