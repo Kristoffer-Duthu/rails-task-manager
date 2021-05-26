5b66d350d7729563858cbede5e15b5b7f225f52c
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-05-26 12:08:50 +0800

    updated the link_to edit and refactored the routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # read all tasks
-  get 'tasks', to: 'tasks#index'
-  # read one task / add a task / create it
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
-  # update one task
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'tasks#update'
-  # delete one task
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
+  # # read all tasks
+  # get 'tasks', to: 'tasks#index'
+  # # read one task / add a task / create it
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # # update one task
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # delete one task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
