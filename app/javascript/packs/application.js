9ef15ea84209b5c37f92a2a61976555a43f43a5f
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-06-02 15:53:16 +0800

    changed some front end - started to add a calendar option

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require mustache/mustache.min
+//= require moment/moment
+//= require clndr/clndr.min
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
